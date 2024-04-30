143072043b01e1937a5dbb07f20a3f78cef14339
Sergio Bono <bonogualtiero@gmail.com>
2024-04-30 16:06:26 +0200

    cutout ontrigger and camera layer

--- Assets/Materials/CutoutRaycast.cs
+++ Assets/Materials/CutoutRaycast.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CutoutObject : MonoBehaviour
@@ -19,22 +20,52 @@ public class CutoutObject : MonoBehaviour
 
     private void Update()
     {
+        /*
+        //    Vector2 cutoutPos = mainCamera.WorldToViewportPoint(targetObject.position);
+        //    cutoutPos.y /= (Screen.width / Screen.height);
+
+        //    Vector3 offset = targetObject.position - transform.position;
+        //    RaycastHit[] hitObjects = Physics.RaycastAll(transform.position, offset, offset.magnitude, wallMask);
+
+        //    for (int i = 0; i < hitObjects.Length; ++i)
+        //    {
+        //        Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
+        //        Debug.Log(hitObjects[i].transform.gameObject.name);
+        //        for (int m = 0; m < materials.Length; ++m)
+        //        {
+
+        //            materials[m].SetVector("_CutoutPos", cutoutPos);
+        //            materials[m].SetFloat("_CutoutSize", 0.25f);
+        //            materials[m].SetFloat("_FalloffSize", 0.25f);
+        //        }
+        //    }*/
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
         Vector2 cutoutPos = mainCamera.WorldToViewportPoint(targetObject.position);
         cutoutPos.y /= (Screen.width / Screen.height);
 
-        Vector3 offset = targetObject.position - transform.position;
-        RaycastHit[] hitObjects = Physics.RaycastAll(transform.position, offset, offset.magnitude, wallMask);
-
-        for (int i = 0; i < hitObjects.Length; ++i)
+        Material r = other.transform.GetComponent<Renderer>().material;
+        if (r != null)
         {
-            Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
-
-            for (int m = 0; m < materials.Length; ++m)
-            {
-                materials[m].SetVector("_CutoutPos", cutoutPos);
-                materials[m].SetFloat("_CutoutSize", 0.25f);
-                materials[m].SetFloat("_FalloffSize", 0.25f);
-            }
+            r.SetVector("_CutoutPos", cutoutPos);
+            r.SetFloat("_CutoutSize", 0.25f);
+            r.SetFloat("_FalloffSize", 0.25f);
         }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Vector2 cutoutPos = mainCamera.WorldToViewportPoint(targetObject.position);
+        cutoutPos.y /= (Screen.width / Screen.height);
+
+        Material r = other.transform.GetComponent<Renderer>().material;
+        if (r != null)
+        {
+            r.SetFloat("_CutoutSize", 0f);
+            r.SetFloat("_FalloffSize", 0f);
+        }
+
     }
 }
--- Assets/Scripts/Sc_PlayerMovement.cs
+++ Assets/Scripts/Sc_PlayerMovement.cs
@@ -131,4 +131,5 @@ public class Sc_PlayerMovement : MonoBehaviour
 
 		}
 	}
+
 }
